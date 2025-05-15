0d590ab00f8ad11ac1365967d8534b104baaa749
Darío Fernández <darioflo19@icloud.com>
2025-05-15 17:22:35 -0600

    Version mas organizada.1

--- src/app/components/seleccionar-cuenta/seleccionar-cuenta.component.ts
+++ src/app/components/seleccionar-cuenta/seleccionar-cuenta.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ObtenerClienteAutenticado } from '../../core/utils/obtener-cliente-autenticado';
 import { NgFor } from '@angular/common';
-import { inversionCompleta } from '../../routes/consulta-inversiones/consulta-inversiones.component';
 
 @Component({
   selector: 'app-seleccionar-cuenta',
